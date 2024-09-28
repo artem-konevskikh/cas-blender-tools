21dcafe6c7fc11a333d7ae3e9d9fa42a5fcaff30
Artem Konevskikh <artem.konevskih@gmail.com>
2024-09-28 18:49:50 +0200

    scan renderer bug fixes

--- blender_render_batch_scans.py
+++ blender_render_batch_scans.py
@@ -27,6 +27,7 @@ Example:
 import os
 from math import radians
 from pathlib import Path
+import sys
 import bpy
 import argparse
 
@@ -36,7 +37,7 @@ def set_render_settings(
     resolution_y: int = 4096,
     file_format: str = "PNG",  # ('PNG', 'OPEN_EXR', 'JPEG')
     color_depth: str = "16",  # ('8', '16')
-    color_mode: str = "RGBA",  # ('RGB', 'RGBA', ...)
+    color_mode: str = "RGB",  # ('RGB', 'RGBA', ...)
 ):
     """
     Set the render settings for a Blender scene.
@@ -53,7 +54,7 @@ def set_render_settings(
     scene = bpy.context.scene
     render = bpy.context.scene.render
 
-    render.engine = 'CYCLES'
+    render.engine = 'BLENDER_EEVEE'
     render.image_settings.color_mode = color_mode
     render.image_settings.color_depth = color_depth
     render.image_settings.file_format = file_format
@@ -97,13 +98,9 @@ def set_render_settings(
         map.use_max = True
         map.max = [255]
 
-    # Set background to black
-    alpha_over = nodes.new(type="CompositorNodeAlphaOver")
-    alpha_over.inputs[1].default_value = (0, 0, 0, 1)  # Black background
+        links.new(render_layers.outputs["Depth"], map.inputs[0])
+        links.new(map.outputs[0], depth_file_output.inputs[0])
 
-    links.new(render_layers.outputs["Depth"], map.inputs[0])
-    links.new(map.outputs[0], alpha_over.inputs[2])
-    links.new(alpha_over.outputs[0], depth_file_output.inputs[0])
     return depth_file_output
 
 
@@ -161,6 +158,56 @@ def load_textured_mesh(model_file: str, max_dim: float = 0.5):
     return obj
 
 
+def set_camera():
+    # Place camera
+    cam = bpy.context.scene.objects['Camera']
+    cam.location = (0, 1, 0.6)
+    cam.data.lens = 35
+    cam.data.sensor_width = 32
+    cam.rotation_euler = (radians(90), 0, radians(180))
+    cam.constraints.new(type='TRACK_TO')
+    cam.constraints['Track To'].target = bpy.data.objects.new("Empty", None)
+    cam.constraints['Track To'].target.location = (0, 0, 0)
+    bpy.context.collection.objects.link(cam.constraints['Track To'].target)
+    cam.constraints['Track To'].track_axis = 'TRACK_NEGATIVE_Z'
+    cam.constraints['Track To'].up_axis = 'UP_Y'
+
+
+def add_lights():
+    # Create lights
+    light_data_1 = bpy.data.lights.new(name="Light1", type='AREA')
+    light_data_1.shape = 'DISK'
+    light_data_1.size = 1
+    light_data_1.energy = 150
+    light_data_1.color = (1, 1, 1)
+    light_data_1.use_shadow = True
+    light_object_1 = bpy.data.objects.new(name="Light1", object_data=light_data_1)
+    light_object_1.location = (0, 0, 2.14)
+    light_object_1.scale = (0.6, 0.6, 0.6)
+    bpy.context.collection.objects.link(light_object_1)
+    
+    light_data_2 = bpy.data.lights.new(name="Light2", type='AREA')
+    light_data_2.shape = 'DISK'
+    light_data_2.size = 1
+    light_data_2.energy = 10
+    light_data_2.color = (1, 1, 1)
+    light_data_2.use_shadow = True
+    light_object_2 = bpy.data.objects.new(name="Light2", object_data=light_data_2)
+    light_object_2.location = (0, 0, 1.45)
+    light_object_2.scale = (2.7, 2.7, 2.7)
+    bpy.context.collection.objects.link(light_object_2)
+    
+    light_data_3 = bpy.data.lights.new(name="Light3", type='AREA')
+    light_data_3.shape = 'DISK'
+    light_data_3.size = 6
+    light_data_3.energy = 10
+    light_data_3.color = (1, 1, 1)
+    light_data_3.use_shadow = True
+    light_object_3 = bpy.data.objects.new(name="Light3", object_data=light_data_3)
+    light_object_3.location = (0, 0, -1.13)
+    bpy.context.collection.objects.link(light_object_3)
+
+
 def rotate360_and_render(    
     output_dir: str,
     rotation_steps: int = 36,
@@ -178,17 +225,28 @@ def rotate360_and_render(
         None
     """
     original_rotation = subject.rotation_euler
+    
     for step_x in range(0, rotation_steps):
         for step_z in range(0, rotation_steps):
             subject.rotation_euler[0] = radians(step_x * (360.0 / rotation_steps))
             subject.rotation_euler[2] = radians(step_z * (360.0 / rotation_steps))
             render_fn = os.path.join(output_dir, f"render_x{step_x}_z{step_z}.jpg")
             depth_fn = os.path.join(output_dir, f"depth_x{step_x}_z{step_z}")
+            # render_fn = f"{output_dir}_render_x{step_x}_z{step_z}.png"
+            # depth_fn = f"{output_dir}_depth_x{step_x}_z{step_z}.png"
             bpy.context.scene.render.filepath = os.path.join(render_fn)
             if depth_file_output:
                 depth_file_output.base_path = os.path.join(depth_fn)
             bpy.ops.render.render(write_still=True)
     subject.rotation_euler = original_rotation
+
+
+def delete_all_meshes():
+    for obj in bpy.context.scene.objects:
+            # Check if the object is a mesh
+            if obj.type == 'MESH':
+                # If it is, delete it
+                bpy.data.objects.remove(obj, do_unlink=True)
 
 
 def render_folder(
@@ -208,30 +266,36 @@ def render_folder(
         None
     """
     models = Path(input_dir).rglob("*.obj")
+    depth_file_output = set_render_settings(width, height)
+    add_lights()
+    set_camera()
+    delete_all_meshes()
     for model in models:
-        depth_file_output = set_render_settings(width, height)
-        obj = load_textured_mesh(str(model), 0.8)
+        if os.path.exists(f"{output_dir}/{model.stem}"):
+            print(f"Skipping {model.stem}")
+            continue
+        
+        print(f"Rendering {model.stem}")
+        obj = load_textured_mesh(str(model), 0.7)
+        
         rotate360_and_render(
             f"{output_dir}/{model.stem}",
-            36,
+            18,
             obj,
             depth_file_output
         )
-        for obj in bpy.context.scene.objects:
-            # Check if the object is a mesh
-            if obj.type == 'MESH':
-                # If it is, delete it
-                bpy.data.objects.remove(obj, do_unlink=True)
+        delete_all_meshes()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Batch render 3D models.")
     parser.add_argument("-i", "--input_dir", type=str, help="Directory containing input .obj files")
-    parser.add_argument("-i", "--output_dir", type=str, help="Directory to save rendered images")
-    parser.add_argument("-w", "--width", type=int, default=4096, help="Width of the rendered images")
-    parser.add_argument("-h", "--height", type=int, default=4096, help="Height of the rendered images")
+    parser.add_argument("-o", "--output_dir", type=str, help="Directory to save rendered images")
+    parser.add_argument("--width", type=int, default=4096, help="Width of the rendered images")
+    parser.add_argument("--height", type=int, default=4096, help="Height of the rendered images")
 
-    args = parser.parse_args()
+    argv = sys.argv[sys.argv.index("--") + 1:]
+    args = parser.parse_args(argv)
 
     input_dir = args.input_dir
     output_dir = args.output_dir
